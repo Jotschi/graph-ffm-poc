2c4459aff23394da9aba88552de282a86475c7d2
Johannes Schüth <git@jotschi.de>
2025-01-23 23:11:06 +0100

    Add pointer handling

--- native-rust-lib/src/lib.rs
+++ native-rust-lib/src/lib.rs
@@ -3,3 +3,18 @@
 pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[no_mangle]
+pub extern "C" fn create_int_pointer(value: i32) -> *mut i32 {
+    let boxed = Box::new(value); // Allocate the value on the heap
+    Box::into_raw(boxed)        // Return a raw pointer
+}
+
+#[no_mangle]
+pub extern "C" fn free_int_pointer(ptr: *mut i32) {
+    if !ptr.is_null() {
+        unsafe {
+            Box::from_raw(ptr); // Reclaim ownership to drop the memory
+        }
+    }
+}
--- src/test/java/io/metaloom/graph/core/ffm/FFMLibraryTest.java
+++ src/test/java/io/metaloom/graph/core/ffm/FFMLibraryTest.java
@@ -3,6 +3,7 @@ package io.metaloom.graph.core.ffm;
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.Linker;
+import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandle;
@@ -26,6 +27,24 @@ public class FFMLibraryTest {
 			.downcallHandle(
 				rustLibrary.findOrThrow("add_numbers"),
 				FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.JAVA_INT, ValueLayout.JAVA_INT));
+
+		MethodHandle createIntPointer = Linker.nativeLinker()
+			.downcallHandle(
+				rustLibrary.findOrThrow("create_int_pointer"),
+				FunctionDescriptor.of(ValueLayout.ADDRESS, ValueLayout.JAVA_INT));
+
+		MethodHandle freeIntPointer = Linker.nativeLinker()
+			.downcallHandle(
+				rustLibrary.findOrThrow("free_int_pointer"),
+				FunctionDescriptor.ofVoid(ValueLayout.ADDRESS));
+
+		// Get a pointer to an int value
+		MemorySegment pointer = (MemorySegment) createIntPointer.invokeExact(42);
+		pointer=pointer.reinterpret(ValueLayout.JAVA_INT.byteSize());
+
+		// Access the value at the pointer
+		int value = pointer.get(ValueLayout.JAVA_INT, 0);
+		System.out.println("Value at pointer: " + value);
 
 		// Call the Rust function
 		int result = (int) addNumbersHandle.invokeExact(5, 7);
